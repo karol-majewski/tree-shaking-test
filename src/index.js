76c9c27b6b5a49f1ec77877e05c5e23790c849dc
Karol Majewski <karol-majewski@users.noreply.github.com>
2019-09-04 16:40:19 +0200

    #1: Baseline

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-import * as option from "fp-ts/es6/Option";
-// import { option } from "fp-ts/es6";
+import { fromNullable } from "fp-ts/es6/Option";
 
-console.log(option.fromNullable(1));
-
-// import { fromNullable } from "fp-ts/es6/Option";
-
-// console.log(fromNullable(1));
+console.log(fromNullable(1));
