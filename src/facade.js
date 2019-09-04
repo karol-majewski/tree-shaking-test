9e800e03daae31e30a1ed148e79b6a44426a65c9
Karol Majewski <karol-majewski@users.noreply.github.com>
2019-09-04 17:06:41 +0200

    #5: Facade without any executed code

--- src/facade.js
+++ src/facade.js
@@ -1,6 +1,4 @@
-import * as everything from 'fp-ts';
+import { alternative } from 'fp-ts';
 import { fromNullable } from "fp-ts/es6/Option";
 
-console.log(everything.either);
-
-export { fromNullable };
+export { fromNullable, alternative };
